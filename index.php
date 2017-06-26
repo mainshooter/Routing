2d8a6e79346cc278a33a28f8993f886292527bbd
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-26 11:10:01 +0200

    Added standaardctrl and to parse the router!

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
   $Router = new Router();
 
   $Router->installedPath = '/leerjaar2/php/Routing/';
+  $Router->standardController = 'standard';
 
   $Router->parseUrl();
   $Router->getController();
   $Router->getMethod();
   $Router->getParameters();
+
+  $Router->parseRouter();
 
   $Router->routerDebug();
 
